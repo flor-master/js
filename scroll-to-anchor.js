6d54f64a5ee42dd6bf12c0643172219bd11f3ce1
Alexandr <Alexandr.Florinskij@yandex.ua>
2013-11-06 15:36:54 +0200

    Update scroll-to-anchor.js
    Scroll the page to the anchor

--- scroll-to-anchor.js
+++ scroll-to-anchor.js
@@ -1,15 +1,14 @@
 /*
 Scroll the page to the anchor
+This script uses JQuery
 */
-	 $("a[href^=#]").click(function (e){
-        e.preventDefault();
-        var href = $(this).attr("href"),
-            target_id = href.substr(1),
-            target_offset = $("#"+target_id).offset().top-47;
-        //if ( target_offset > 200 )
-          //  target_offset = target_offset;
-
-        $('html, body').animate({
-            scrollTop: target_offset
-        });
-    });
+ $("a[href^=#]").click(function (e){
+	e.preventDefault();
+	var href = $(this).attr("href"),
+	    target_id = href.substr(1),
+	    target_offset = $("#"+target_id).offset().top;
+	
+	$('html, body').animate({
+	    scrollTop: target_offset
+	});
+});
